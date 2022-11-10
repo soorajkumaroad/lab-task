656d3007da3257962f93af88814f30e7a14c4490
soorajkumaroad <117901629+soorajkumaroad@users.noreply.github.com>
2022-11-10 01:23:40 -0800

    Add files via upload
    work done

---
+++ labtast1.java
@@ -0,0 +1,15 @@
+f public class labtast1 {
+    public static void main(String[] args) {
+        int mySetNo = 73;              
+        float myNumber = 0319.04f;    
+        char myLetter = 'S';         
+        boolean mybool = true;       
+        String myText = "My Name Is sooraj kumar
+        System.out.println(mySetNo);
+        System.out.println(myNumber);
+        System.out.println(myLetter);
+        System.out.println(mybool);
+        System.out.println(myText);
+    }
+}
+
